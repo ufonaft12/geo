2ded6b765269560883a1612c2284c57b644386d9
hikaram <hikaram@gmail.com>
2018-03-14 12:22:37 +0200

    Add contact page

---
+++ contacts.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="he-IL">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="favicon.ico">
+		
+		<title>GEO - front page</title>
+		
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="css/bootstrap-grid.min.css">
+		
+		<!-- Custom styles for this template -->
+		<link rel="stylesheet" href="css/owl.carousel.min.css">
+		<link rel="stylesheet" href="css/owl.theme.default.min.css">
+		<link rel="stylesheet" href="css/linearicons-free.min.css">
+		<link rel="stylesheet" href="css/opensanshebrew.min.css">
+		<link rel="stylesheet" href="css/fontello.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/front-page.css">
+	</head>
+	
+	<body class="rtl home front-page">
+		<header class="container-fluid">
+			<div class="row">
+				<div class="container"> 
+			<div class="row justify-content-start">
+				<div class="col-md-auto">
+					<ul class="header_links">
+						<li><a href="#">ENG</a></li>
+						<li><a href="#">Geo-SIM</a></li>
+						<li><a href="#"><i class="icon-linkedin"></i></a></li>
+						<li><a href="#"><i class="icon-facebook"></i></a></li>
+						<li><a href="tel:03-7682222">03-7682222<i class="icon-phone"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<!-- /.row -->
+			
+			
+			<div class="row">
+				
+				<div class="header_menu col-md-auto">
+					<div class="search">
+						<img src="img/search_icon.png" alt="">
+					</div>
+					<a class="mobile-menu-btn" href="#menu">
+						<span></span>
+						<span></span>
+						<span></span>
+					</a>
+					<div id="menu">
+						<div class="header_container">
+							<ul id="menu_header" class="header_menu-menu">
+								<li class="menu_item menu_item_has_children">
+									<a href="#/">עמוד הבית</a>
+									<ul class="sub_menu">
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+									</ul>
+								</li>
+								<li class="menu_item menu_item_has_children">
+									<a href="#">אודות</a>
+									<ul class="sub_menu">
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+									</ul>
+								</li>
+								<li class="menu_item menu_item_has_children">
+									<a href="#/">תחומי פעילות</a>
+									<ul class="sub_menu">
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+										<li class="sub-menu_item">
+											<a href="#/">אוהל ענק להשכרה</a>
+										</li>
+									</ul>
+								</li>
+								<li class="menu_item menu_item_has_children">
+									<a href="#/">שיטות מחקר</a>
+								</li>
+								<li class="menu_item menu_item_has_children">
+									<a href="#/">מהתקשורת</a>
+								</li>
+								<li class="menu_item">
+									<a href="#/">מאמרים</a>
+								</li>
+								<li class="menu_item">
+									<a href="#/">צור קשר</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- /.menu -->
+					
+					<!-- /.search -->
+				</div>
+				<!-- /.header-menu -->
+				<div class="col-md">
+					<div class="header_logo">
+						<a href="" title="GEO - front page"><img src="img/logo.png" alt=""></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /.row -->
+		</div>
+		</div>
+	</header>
+	
+	
+			
+			
+			<!-- Bootstrap core JavaScript ================================================== -->
+			<!-- Placed at the end of the document so the pages load faster -->
+			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+			<script>window.jQuery || document.write('<script src="js/jquery-3.3.1.slim.min.js"><\/script>')</script>
+			<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+			<script type="text/javascript" src="js/main.js"></script>
+			<script type="text/javascript" src="js/beetle.js"></script>
+		</body>
+	</html>
