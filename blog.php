a3851e455a977be38f75e31666f0f4ef282f231f
Gavrilovich Alexander <gavrilovich.a@gmail.com>
2018-03-15 14:20:56 +0200

    add blog.php

---
+++ blog.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="he-IL">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>GEO - blog page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/linearicons-free.min.css">
+    <link rel="stylesheet" href="css/opensanshebrew.min.css">
+    <link rel="stylesheet" href="css/fontello.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/blog.css">
+    <link rel="stylesheet" href="css/front-page.css">
+</head>
+
+<body class="rtl home blog-page">
+<header class="container-fluid">
+    <div class="row">
+        <div class="container">
+            <div class="row justify-content-start">
+                <div class="col-md-auto">
+                    <ul class="header_links">
+                        <li><a href="#">ENG</a></li>
+                        <li><a href="#">Geo-SIM</a></li>
+                        <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                        <li><a href="tel:03-7682222">03-7682222<i class="icon-phone"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /.row -->
+
+
+            <div class="row">
+
+                <div class="header_menu col-md-auto">
+                    <div class="search">
+                        <img src="img/search_icon.png" alt="">
+                    </div>
+                    <a class="mobile-menu-btn" href="#menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
+                    <div id="menu">
+                        <div class="header_container">
+                            <ul id="menu_header" class="header_menu-menu">
+                                <li class="menu_item menu_item_has_children">
+                                    <a href="#/">עמוד הבית</a>
+                                    <ul class="sub_menu">
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu_item menu_item_has_children">
+                                    <a href="#">אודות</a>
+                                    <ul class="sub_menu">
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu_item menu_item_has_children">
+                                    <a href="#/">תחומי פעילות</a>
+                                    <ul class="sub_menu">
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                        <li class="sub-menu_item">
+                                            <a href="#/">אוהל ענק להשכרה</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu_item menu_item_has_children">
+                                    <a href="#/">שיטות מחקר</a>
+                                </li>
+                                <li class="menu_item menu_item_has_children">
+                                    <a href="#/">מהתקשורת</a>
+                                </li>
+                                <li class="menu_item">
+                                    <a href="#/">מאמרים</a>
+                                </li>
+                                <li class="menu_item">
+                                    <a href="#/">צור קשר</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /.menu -->
+
+                    <!-- /.search -->
+                </div>
+                <!-- /.header-menu -->
+                <div class="col-md">
+                    <div class="header_logo">
+                        <a href="" title="GEO - front page"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    </div>
+</header>
+
+<section class="breadcrumbs-box container">
+    <div class="col-md-12">
+        <div class="dimox_breadcrumbs">
+            <div class="breadcrumbs">
+                <span>
+                    <a href="#">
+                        <span>מאמרים</span>
+                    </a>
+                </span>
+                <span class="sep">|</span>
+                <span class="current">עמוד הבית</span>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="s-blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="blog-item_head">
+                    <h2>מאמרים</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3 right-side">
+                        <div class="blog-item">
+                            123
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 center-block">
+                        <div class="blog-item">
+                            <div class="item-img-wrap">
+                                <img src="img/post1.png" alt="post1">
+                            </div>
+
+                            <div class="item-center-content">
+                                <span>מאת: פרופ׳ אבי דגני</span>
+                                <h5>לקוח מרוצה זה כבר לא מספיק! </h5>
+                                <p>איזו חברה תנצח בקרב השיווק בתחומה? מדוע הצרכן מעדיף לקנות בחנות מסוימת? מה עושה את ההבדל? מה הופך את הלקוח לנאמן? | מתקופה לתקופה משתנות אופנות שוק ונסיבות כלכליות, צצות מגמות חברתיות ותרבותיות חדשות המייצרות אפיוני-צריכה, והשאלות הקלאסיות הללו שבות ועולות.</p>
+                                <div class="info1 buttons">
+                                    <a href="#" class="button button-active">המשך לקרוא</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 left-side">
+                        <div class="blog-item">
+                            <div class="top-line">
+                                <h5>ארכיון</h5>
+                                <p>2015</p>
+                                <p>2013</p>
+                                <p>2012</p>
+                            </div>
+
+                            <div class="bottom-line">
+                                <h5>תגיות</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="form container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>צור קשר</h2>
+            <form action="/#wpcf7-f549-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+
+                <p>
+								<span class="wpcf7-form-control-wrap your-name">
+									<input name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="שם מלא *" type="text">
+								</span>
+
+                    <span class="wpcf7-form-control-wrap your-email">
+									<input name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-email" aria-invalid="false" placeholder="מייל *" type="email">
+								</span>
+
+                    <span class="wpcf7-form-control-wrap tel-928">
+									<input name="tel-928" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-tel" aria-invalid="false" placeholder="טלפון *" type="tel">
+								</span>
+
+                    <span class="wpcf7-form-control-wrap text-893">
+									<input name="text-893" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="חברה" type="text">
+								</span>
+
+                    <span class="wpcf7-form-control-wrap your-message">
+									<textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="תוכן ההודעה:"></textarea>
+								</span>
+                </p>
+
+                <p>
+                    <input value="שלח" class="wpcf7-form-control wpcf7-submit" type="submit">
+                    <span class="ajax-loader"></span>
+                </p>
+
+            </form>
+        </div>
+    </div>
+</section>
+
+<footer class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="container">
+                <div class="footer_row row">
+                    <div class="col-md-8">
+                        <ul id="menu_footer" class="footer_menu-menu">
+                            <li class="menu-item">
+                                <a href="#/">צור קשר</a>
+                            </li>
+                            <li class="menu_item">
+                                <a href="#">דרושים</a>
+                            </li>
+                            <li class="menu_item">
+                                <a href="#/">מאמרים</a>
+                            </li>
+                            <li class="menu_item">
+                                <a href="#/">מהתקשורת</a>
+                            </li>
+                            <li class="menu_item">
+                                <a href="#/">שיטות מחקר</a>
+                            </li>
+                            <li class="menu_item">
+                                <a href="#/">תחומי פעילות</a>
+                            </li>
+                            <li class="menu_item">
+                                <a href="#/">אודות</a>
+                            </li>
+                            <li class="menu_item">
+                                <a href="#/">עמוד הבית</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer_social col-md-4">
+                        <ul class="footer_links">
+                            <li><i class="icon-facebook"></i></li>
+                            <li><i class="icon-linkedin"></i></li>
+                            <li><a href="tel:03-7682222">03-7682222<i class="icon-phone"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="copyright col-md-12">
+                        ©  כל הזכויות שמורות, קבוצת גיאוקרטוגרפיה בע״מ, 2016
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</footer>
+<!-- Bootstrap core JavaScript ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-3.3.1.slim.min.js"><\/script>')</script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+<script type="text/javascript" src="js/beetle.js"></script>
+</body>
+</html>
