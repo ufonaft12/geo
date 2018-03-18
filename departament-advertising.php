cb6868ac8b76a3dd4dfb15d08c79a2e427e91e42
killerycya <singularity123097@gmail.com>
2018-03-18 11:10:46 +0200

    dg

---
+++ departament-advertising.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="he-IL">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="favicon.ico">
+
+		<title>GEO - departament advertising</title>
+
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="css/bootstrap-grid.min.css">
+
+		<!-- Custom styles for this template -->
+		<link rel="stylesheet" href="css/owl.carousel.min.css">
+		<link rel="stylesheet" href="css/owl.theme.default.min.css">
+		<link rel="stylesheet" href="css/linearicons-free.min.css">
+		<link rel="stylesheet" href="css/opensanshebrew.min.css">
+		<link rel="stylesheet" href="css/fontello.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/departament-advertising.css">
+	</head>
+
+	<body class="rtl departament-adv-page">
+	<header class="container-fluid">
+			<div class="row">
+				<div class="container">
+					<div class="row justify-content-start">
+						<div class="col-md-auto">
+							<ul class="header_links">
+								<li><a href="#">ENG</a></li>
+								<li><a href="#">Geo-SIM</a></li>
+								<li><a href="#"><i class="icon-linkedin"></i></a></li>
+								<li><a href="#"><i class="icon-facebook"></i></a></li>
+								<li><a href="tel:03-7682222">03-7682222<i class="icon-phone"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+
+				  <div class="row">
+
+						<div class="header_menu col-md-auto">
+							<div class="search">
+								<img src="img/search_icon.png" alt="">
+							</div>
+							<a class="mobile-menu-btn" href="#menu">
+								<span></span>
+								<span></span>
+								<span></span>
+							</a>
+							<div id="menu">
+								<div class="header_container">
+									<ul id="menu_header" class="header_menu-menu">
+										<li class="menu_item menu_item_has_children">
+											<a href="#/">עמוד הבית</a>
+											<ul class="sub_menu">
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+											</ul>
+										</li>
+										<li class="menu_item menu_item_has_children">
+											<a href="#">אודות</a>
+											<ul class="sub_menu">
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+											</ul>
+										</li>
+										<li class="menu_item menu_item_has_children">
+											<a href="#/">תחומי פעילות</a>
+											<ul class="sub_menu">
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+												<li class="sub-menu_item">
+													<a href="#/">אוהל ענק להשכרה</a>
+												</li>
+											</ul>
+										</li>
+										<li class="menu_item menu_item_has_children">
+											<a href="#/">שיטות מחקר</a>
+										</li>
+										<li class="menu_item menu_item_has_children">
+											<a href="#/">מהתקשורת</a>
+										</li>
+										<li class="menu_item">
+											<a href="#/">מאמרים</a>
+										</li>
+										<li class="menu_item">
+											<a href="#/">צור קשר</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+
+						</div>
+						<div class="col-md">
+							<div class="header_logo">
+								<a href="" title="GEO - front page"><img src="img/logo.png" alt=""></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</header>
+
+
+	<section class="breadcrumbs-box container">
+	    <div class="col-md-12">
+	        <div class="dimox_breadcrumbs">
+	            <div class="breadcrumbs">
+	                <span>
+	                    <a href="#">
+	                        <span>עמוד הבית</span>
+	                    </a>
+	                </span>
+	                <span class="sep">|</span>
+                    <span>
+	                    <a href="#">
+	                        <span> תחומי פעילות</span>
+	                    </a>
+	                </span>
+                    <span class="sep">|</span>
+	                <span class="current"> שיווק, פרסום ודעת קהל</span>
+	            </div>
+	        </div>
+	    </div>
+	</section>
+
+    <!-- שיווק, פרסום ודעת קהל -->
+    <section class = "container adv-text-block">
+        <div class = "col-md-12">
+            <h2 class = "adv-title">
+                    שיווק, פרסום ודעת קהל
+            </h2>
+
+            <div class = "adv-text-block-description">
+                <p>
+                    גיאוקרטוגרפיה מעמידה לרשות לקוחותיה מגוון רחב של פתרונות מחקר שיווקי, ככלי תומך החלטות, בגיבוש אסטרטגיה שיווקית-תקשורתית: מודלים לבחינת מעמד מותג ונכסיות מותג, בחינת קונספטים, בחינת פרזנטורים, מיפוי תהליך קבלת החלטות של צרכנית, אפקטיביות פרסום, ניתוח עולם התחרות ועוד.
+                </p>
+
+                <p>
+                    המחקרים נערכים בקרב מגוון מגזרי האוכלוסייה השונים, כולל אוכלוסיות מקצועיות, תוך התאמת המתודולוגיה, שיטת הדגימה ושיטת התשאול לקהלי המטרה ולמטרת המחקר.
+ע
+                </p>
+
+                <p>
+                    עבודת המחקר מאופיינת בניתוחים ייחודיים וביניהם: ניתוח סגמנטלי ומגזרי, ניתוח גיאוגרפי וניתוח סוציו-אקונומי מורחב, המאפשרים הפקת משמעותיות ומסקנות אופרטיביות, כחלק מתהליך מובנה שמטרתו ליצר ערך ללקוח.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Accordeon -->
+    <section class = "departament-adv-accordeon">
+        <div class = "acc-item">
+            <div class = "container justify-content-md-center">
+                <div class = "col-md-10">
+                    <div class = "acc-heading">
+                        <div class = "acc-text">
+                            מעמד שיווקי ונכסיות מותג (Brand Equity)
+
+                        </div>
+                        <div class = "acc-arrow">
+                            <svg width ="22px" height = "22px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 612 612" style="enable-background:new 0 0 612 612;" xml:space="preserve">
+                                <g>
+                                    <g id="_x31_0_34_">
+                                        <g>
+                                            <path d="M604.501,134.782c-9.999-10.05-26.222-10.05-36.221,0L306.014,422.558L43.721,134.782     c-9.999-10.05-26.223-10.05-36.222,0s-9.999,26.35,0,36.399l279.103,306.241c5.331,5.357,12.422,7.652,19.386,7.296     c6.988,0.356,14.055-1.939,19.386-7.296l279.128-306.268C614.5,161.106,614.5,144.832,604.501,134.782z" style="&#10;    color: blue;&#10;    background: blue;&#10;    fill: #d02230;&#10;"/>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                    </div>
+                    <div class = "acc-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="container">
+					<div class="footer_row row">
+						<div class="col-md-8">
+							<ul id="menu_footer" class="footer_menu-menu">
+								<li class="menu-item">
+									<a href="#/">צור קשר</a>
+								</li>
+								<li class="menu_item">
+									<a href="#">דרושים</a>
+								</li>
+								<li class="menu_item">
+									<a href="#/">מאמרים</a>
+								</li>
+								<li class="menu_item">
+									<a href="#/">מהתקשורת</a>
+								</li>
+								<li class="menu_item">
+									<a href="#/">שיטות מחקר</a>
+								</li>
+								<li class="menu_item">
+									<a href="#/">תחומי פעילות</a>
+								</li>
+								<li class="menu_item">
+									<a href="#/">אודות</a>
+								</li>
+								<li class="menu_item">
+									<a href="#/">עמוד הבית</a>
+								</li>
+							</ul>
+						</div>
+						<div class="footer_social col-md-4">
+							<ul class="footer_links">
+								<li><i class="icon-facebook"></i></li>
+								<li><i class="icon-linkedin"></i></li>
+								<li><a href="tel:03-7682222">03-7682222<i class="icon-phone"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="row">
+						<div class="copyright col-md-12">
+							©  כל הזכויות שמורות, קבוצת גיאוקרטוגרפיה בע״מ, 2016
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</footer>
+	<!-- Bootstrap core JavaScript ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-3.3.1.slim.min.js"><\/script>')</script>
+
+    </body>
+</html>
